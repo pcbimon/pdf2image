d2aab77e4e70a30d5ae7901b4590281405497b46
Patipat Chewprecha <patipat.che@mahidol.edu>
2021-01-13 21:13:16 +0700

    init

---
+++ pdf2image/PdfLibrary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using PDFiumSharp;
+
+namespace pdf2image
+{
+    public static class Converter
+    {
+        private const double Scale = 4.166666666666667;
+
+        private static byte[] BmpToPng(byte[] bmp)
+        {
+            using var stream = new MemoryStream(bmp);
+            using var bitmap = Bitmap.FromStream(stream);
+            using var outStream = new MemoryStream();
+            bitmap.Save(outStream, ImageFormat.Png);
+            return outStream.ToArray();
+        }
+
+        public static string[] PdfToPng(string pdfPath, string targetDirectory)
+        {
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            var outputs = new List<string>();
+            var index = 0;
+
+            using var doc = new PdfDocument(pdfPath);
+
+            foreach (var page in doc.Pages)
+            {
+                var width = (int)(page.Width * Scale);
+                var heigh = (int)(page.Height * Scale);
+                var outputPath = Path.Combine(targetDirectory, $"{index++}.png");
+
+                using var bitmap = new PDFiumBitmap(width, heigh, true);
+                using var stream = new MemoryStream();
+
+                page.Render(bitmap);
+                bitmap.Save(stream, 300, 300);
+
+                byte[] png = BmpToPng(stream.ToArray());
+                File.WriteAllBytes(outputPath, png);
+
+                outputs.Add(outputPath);
+            }
+            return outputs.ToArray();
+        }
+    }
+}
---
+++ pdf2image/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace pdf2image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte[] getpdf = File.ReadAllBytes(Path.Combine(System.IO.Directory.GetParent(@"./").Parent.Parent.Parent.ToString(),"test.pdf"));
+            string path = Path.Combine(System.IO.Directory.GetParent(@"./").Parent.Parent.Parent.ToString(), "test.pdf");
+            var outputs = Converter.PdfToPng(path, "__output__");
+            foreach (var item in outputs)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
